9b5ef5df94301d141311be0f5f310923d93cb3a5
Alessandro Palumbi <alesspal@Mac-mini-de-Alessandro.local>
2023-03-16 18:19:08 +0100

    initial code

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: alesspal <alesspal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:28:45 by alesspal          #+#    #+#             */
-/*   Updated: 2023/03/16 17:15:01 by alesspal         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:17:38 by alesspal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,3 @@
 #include <signal.h>
 #include <unistd.h>
 #include "../libft/libft.h"
-
-typedef enum
-{
-	E_ECHO,				// echo
-	CD,					// cd
-	PWD,				// pwd
-	EXPORT,				// export
-	UNSET,				// unset
-	ENV,				// env
-	EXIT,				// exit
-	INPUT_REDIRECT,		// <
-	OUTPUT_REDIRECT,	// >
-	HEREDOC_REDIRECT,	// <<
-	APPEND_REDIRECT,	// >>
-	NOTHING
-} cmd_type;
-
-typedef struct s_token
-{
-	cmd_type cmd;
-	char *arg;
-	struct s_token *next;
-} t_token;
-
-// laxer
-t_token	laxing_cmd(char *input);
-
-// expender
-t_token	expend_cmd(t_token token);
-
-// executor
-void	execute_cmd(char *input);
-
-// cmd
-// ...
-// ...
-
-// signal managemenent
-void	sigINGORE_handler(int sig);
-void	sigINT_handler(int signum);
-int		init_signal(int signum, void(*handler)(int));
