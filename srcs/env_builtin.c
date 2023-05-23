445f66e7bf6ffebaf389342eab723bbe2b3c1651
Eric Holzer <eric.holzer.lsn@gmail.com>
2023-05-23 16:07:50 +0200

    Fix env

--- srcs/env_builtin.c
+++ srcs/env_builtin.c
@@ -3,20 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   env_builtin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eholzer <eholzer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 10:43:34 by eholzer           #+#    #+#             */
-/*   Updated: 2023/05/22 15:25:22 by eholzer          ###   ########.fr       */
+/*   Updated: 2023/05/23 16:06:45 by eric             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "env_utils.h"
 
 void	env(t_data *data)
 {
 	int	i;
 
 	i = -1;
+	if (ft_find_index_env("PATH", data->envp_cpy) == -1)
+	{
+		printf("minishell: env: command not found\n");
+		exit(127);
+	}
 	while (data->envp_cpy[++i])
 		printf("%s\n", data->envp_cpy[i]);
 	exit(0);
